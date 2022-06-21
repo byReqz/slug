8d52fc9e2ca89ebe4730a2c6e983f680070533d6
Nils <nils@byreqz.de>
2022-06-21 23:25:13 +0200

    add more helper functions and suffix support

--- slug.go
+++ slug.go
@@ -3,9 +3,11 @@ package slug
 import (
 	"fmt"
 	"github.com/fatih/color"
+	"os"
+	"time"
 )
 
-var DefaultLogger Logger
+var DefaultLogger *Logger // logger using default configuration
 
 var (
 	DebugLevel   = -1
@@ -15,64 +17,117 @@ var (
 )
 
 type Logger struct {
-	Enabled       bool
-	Color         bool
-	Level         int
-	DebugFormat   string
-	DebugPrefix   string
-	InfoFormat    string
-	InfoPrefix    string
-	WarningFormat string
-	WarningPrefix string
-	ErrorFormat   string
-	ErrorPrefix   string
+	Enabled       bool   // state of logger
+	Color         bool   // whether to use colors
+	Level         int    // the minimum log level the logger will output
+	DebugFormat   string // formatstring for debug level logs
+	DebugPrefix   string // message prefix for debug level logs
+	DebugSuffix   string // message suffix for debug level logs
+	InfoFormat    string // formatstring for info level logs
+	InfoPrefix    string // message prefix for info level logs
+	InfoSuffix    string // message suffix for info level logs
+	WarningFormat string // formatstring for warning level logs
+	WarningPrefix string // message prefix for warning level logs
+	WarningSuffix string // message suffix for warning level logs
+	ErrorFormat   string // formatstring for error level logs
+	ErrorPrefix   string // message prefix for error level logs
+	ErrorSuffix   string // message suffix for error level logs
 }
 
+func init() {
+	DefaultLogger = NewLogger() // set DefaultLogger
+}
+
+// NewLogger returns a new default Logger and should be used in most cases
 func NewLogger() *Logger {
 	var l Logger
 	l.Enabled = true
 	l.Color = true
 	l.DebugFormat = "%s "
-	l.DebugPrefix = color.CyanString("Debug:")
+	l.DebugPrefix = color.MagentaString("Debug:")
+	l.DebugSuffix = "| " + fmt.Sprint(time.Now().Round(time.Second))
 	l.InfoFormat = "%s "
-	l.InfoPrefix = color.MagentaString("Info:")
+	l.InfoPrefix = color.CyanString("Info:")
+	l.InfoSuffix = "| " + fmt.Sprint(time.Now().Round(time.Second))
 	l.WarningFormat = "%s "
 	l.WarningPrefix = color.YellowString("Warning:")
+	l.WarningSuffix = "| " + fmt.Sprint(time.Now().Round(time.Second))
 	l.ErrorFormat = "%s "
 	l.ErrorPrefix = color.RedString("Error:")
+	l.ErrorSuffix = "| " + fmt.Sprint(time.Now().Round(time.Second))
 	return &l
 }
 
-func (l *Logger) Println(v ...any) {
+func (l Logger) Println(v ...any) {
 
 }
 
-func (l *Logger) Sprintln(v ...any) string {
+func (l Logger) Sprintln(v ...any) string {
 	return ""
 }
 
-func (l *Logger) Debug(v ...any) {
+// Debug prints a log-entry at debug level
+func (l Logger) Debug(v ...any) {
 	if l.Level > -1 {
 		return
 	}
 	fmt.Print(l.SDebugln(v...))
 }
 
-func (l *Logger) SDebugln(v ...any) string {
+// SDebugln returns a log-entry at debug level with a newline at the end
+func (l Logger) SDebugln(v ...any) string {
 	if l.Level > -1 {
 		return ""
 	}
-	format := l.DebugFormat
-	for _, _ = range v {
-		format = format + "%v "
+	for range v {
+		l.DebugFormat = l.DebugFormat + "%v "
 	}
-	return l.SDebugf(format, v...) + "\n"
+	l.DebugFormat = l.DebugFormat + "%s"
+	return l.SDebugf(l.DebugFormat, v...) + "\n"
 }
 
-func (l *Logger) SDebugf(format string, v ...any) string {
+// SDebugf returns a log-entry at debug level following the format f
+func (l Logger) SDebugf(format string, v ...any) string {
 	if l.Level > -1 {
 		return ""
 	}
 	p := append([]any{l.DebugPrefix}, v...)
+	p = append(p, l.DebugSuffix)
 	return fmt.Sprintf(format, p...)
+}
+
+// Error prints a log-entry at error level
+func (l Logger) Error(v ...any) {
+	if l.Level > -1 {
+		return
+	}
+	fmt.Print(l.SErrorln(v...))
+}
+
+// SErrorln returns a log-entry at error level with a newline at the end
+func (l Logger) SErrorln(v ...any) string {
+	if l.Level > -1 {
+		return ""
+	}
+	for range v {
+		l.ErrorFormat = l.ErrorFormat + "%v "
+	}
+	l.ErrorFormat = l.ErrorFormat + "%s"
+	return l.SErrorf(l.ErrorFormat, v...) + "\n"
+}
+
+// SErrorf returns a log-entry at error level following the format f
+func (l Logger) SErrorf(format string, v ...any) string {
+	if l.Level > -1 {
+		return ""
+	}
+	p := append([]any{l.ErrorPrefix}, v...)
+	p = append(p, l.ErrorSuffix)
+	return fmt.Sprintf(format, p...)
+}
+
+// Fatal prints a log-entry at error level and exits with 1
+func (l Logger) Fatal(v ...any) {
+	l.Error(v...)
+	os.Exit(1)
 }
